5b50688ed303474b951880be544521a0b3d1e007
om mistry <ommistry2914@gmail.com>
2025-03-20 23:26:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './Pages/Navbar'
+
+function App() {
+
+  return (
+    <>
+        <div>
+          <Navbar/>
+        </div>
+    </>
+  )
+}
+
+export default App
